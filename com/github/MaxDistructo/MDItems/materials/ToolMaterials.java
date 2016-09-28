633d9aa94c9db9b2320f5d3b5dcdccea12962395
MaxDistructo <MaxDistructo@users.noreply.github.com>
2016-09-28 12:19:06 -0500

    Add files via upload

---
+++ com/github/MaxDistructo/MDItems/materials/ToolMaterials.java
@@ -0,0 +1,9 @@
+package com.github.MaxDistructo.MDItems.materials;
+
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class ToolMaterials {
+    public static ToolMaterial dragon = EnumHelper.addToolMaterial("dragon", 14, 10000, 15.0F, 16.0F ,50);
+    public static ToolMaterial wither = EnumHelper.addToolMaterial("wither", 14, 5000, 15.0F, 8.5F, 50);
+}
